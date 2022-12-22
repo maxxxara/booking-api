d92feda9487f92d0b5dd3c36cd3b91576ffd4512
maxxxara <dati.maxarashvili24@gmail.com>
2022-12-23 00:38:35 +0400

    cors problem fix 1.1

--- index.js
+++ index.js
@@ -15,28 +15,28 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true)
     next()
 })
-const corsOpts = {
-    origin: 'https://movie-booking-api.vercel.app/',
+// const corsOpts = {
+//     origin: 'https://movie-booking-api.vercel.app/',
   
-    methods: [
-      'GET',
-      'POST',
-    ],
+//     methods: [
+//       'GET',
+//       'POST',
+//     ],
   
-    allowedHeaders: [
-      'Content-Type',
-    ],
-};
+//     allowedHeaders: [
+//       'Content-Type',
+//     ],
+// };
   
-app.use(cors(corsOpts));
+app.use(cors());
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 
 
 app.use("/movies/", moviesRoutes);
