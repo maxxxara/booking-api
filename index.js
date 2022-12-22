7676729ac87feab52c96cb61a5960ea8db5e9ba3
maxxxara <dati.maxarashvili24@gmail.com>
2022-12-23 00:41:56 +0400

    1.4

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 // });
 
 var corsOptions = {
-    origin: 'https://movie-booking-api.vercel.app/',
+    origin: 'https://movie-booking-api.vercel.app/movies/newest',
     optionsSuccessStatus: 200 // For legacy browser support
 }
 
