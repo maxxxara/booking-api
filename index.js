71ea278516372431d46ddb7fc4eebbb0003e1db8
maxxxara <dati.maxarashvili24@gmail.com>
2022-12-23 00:43:46 +0400

    1.6

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 //     ],
 // };
   
-// app.use(cors());
+app.use(cors());
 
 // app.use(function(req, res, next) {
 //     res.setHeader('Access-Control-Allow-Origin', '*');
