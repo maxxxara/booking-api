697fe4e245361109ea8e92af34fd91350186b3a6
maxxxara <dati.maxarashvili24@gmail.com>
2022-12-23 11:38:17 +0400

    day2.1

--- index.js
+++ index.js
@@ -27,13 +27,13 @@ app.use(Express.json())
   
 // app.use(cors());
 
-// app.use(function(req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 
 app.use("/movies/", moviesRoutes);
