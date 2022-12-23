59522c03f58a7bf118a4eeef41c4287dd6bd5d9b
maxxxara <dati.maxarashvili24@gmail.com>
2022-12-23 11:43:46 +0400

    day2.2

--- index.js
+++ index.js
@@ -25,25 +25,23 @@ app.use(Express.json())
 //     ],
 // };
   
-// app.use(cors());
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+})
+app.use(cors())
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 app.use("/movies/", moviesRoutes);
 app.use("/calendar/", calendarRoutes);
