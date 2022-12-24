49236f17d7e8bedc014d56173d3db9157bf45a40
maxxxara <dati.maxarashvili24@gmail.com>
2022-12-24 21:10:10 +0400

    asd

--- index.js
+++ index.js
@@ -14,9 +14,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true)
     next()
 })
-app.use(cors({
-    origin: "http://localhost:3000"
-}))
+app.use(cors())
 
 
 app.use("/movies/", moviesRoutes);
