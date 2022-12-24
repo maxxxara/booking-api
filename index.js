15f0da2fd4e8f51e5334578b54f8c55c2326547d
maxxxara <dati.maxarashvili24@gmail.com>
2022-12-24 22:00:24 +0400

    2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true)
     next()
 })
-app.use(cors({credentials: true, origin: ['http://localhost:3000', 'http://localhost:3001']}));
+app.use(cors({credentials: true, origin: ['http://localhost:3000', 'https://movie-booking-delta.vercel.app/']}));
 
 
 
