264b0357976804c10b0dd9553f7e6f2c1310d4e0
maxxxara <dati.maxarashvili24@gmail.com>
2024-11-13 21:49:19 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
 import Express from "express";
 const app = new Express();
-import moviesRoutes from "./routes/movies.js";
-import calendarRoutes from "./routes/calendar.js";
-import seatsRoutes from "./routes/seats.js";
-import chatRoutes from "./routes/chat.js";
-import cors from "cors";
-
 
 app.use(Express.json())
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://movie-booking-delta.vercel.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-app.use(cors({credentials: true, origin: 'https://movie-booking-delta.vercel.app'}));
-
-
-
-app.use("/movies/", moviesRoutes);
-app.use("/calendar/", calendarRoutes);
-app.use("/seats/", seatsRoutes);
-app.use("/chat/", chatRoutes);
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 const PORT = process.env.PORT || 5050;
-app.listen(PORT, () => {
+app.listen(Number(PORT), () => {
     console.log("API Working");
 })
